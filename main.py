67506de7fa98c8e9582875aee6fc0205cd822e36
mozgoder69 <mozgoder69@gmail.com>
2024-03-03 19:27:32 +0200

    The First Encounter

---
+++ main.py
@@ -0,0 +1,130 @@
+# Стандартные библиотеки
+from http import HTTPStatus
+from typing import Optional
+
+# Сторонние библиотеки
+from fastapi import FastAPI, HTTPException, Request, Depends
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pydantic import ValidationError
+import uvicorn
+
+# Локальные модули
+from pg_con import gen_con, auth_user
+from pg_fun import get_selectable_tables, get_table_columns, get_table_records
+import views
+import domain
+
+# Создайте основное приложение FastAPI
+app = FastAPI()
+views = Jinja2Templates(directory="views")
+static = StaticFiles(directory="views/static")
+app.mount("/static", static, name="static")
+
+@app.post("/auth")
+async def auth(form_data: OAuth2PasswordRequestForm = Depends(), is_guest=False):
+    # try:
+    #     jwt_token = await auth_user(form_data.username, form_data.password, is_guest)
+    # except Exception as e:
+    #     raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail=f"Incorrect username or password: {e}")
+
+    jwt_token = await auth_user(form_data.username, form_data.password, is_guest)
+
+    return response
+
+@app.get("/api/fields/{table_name}")
+async def api_fields(request: Request, table_name):
+    columns_data = await get_columns(table_name)
+    columns = [dict(record) for record in columns_data["table_columns"]]  # Преобразование каждой записи в словарь
+
+    for column in columns:
+        column["html_input_type"] = db_type_to_input(column["ctype"])
+
+    icon_name = db_table_to_icon(table_name)
+
+    # Рендерим HTML-форму с использованием Jinja2
+    return views.TemplateResponse("form_template.html", {"request": request, "icon_name": icon_name, "table_name": table_name, "columns": columns})
+
+
+@app.get("/api/tables")
+async def api_tables():
+    return await get_tables()
+
+@app.get("/api/columns/{table_name}")
+async def api_columns(table_name):
+    return await get_columns(table_name)
+
+@app.get("/api/records/{table_name}")
+async def api_records(table_name):
+    return await get_records(table_name)
+
+
+#
+
+
+@app.get('/table={table_name}/select_one/id={obj_id}', response_class=HTMLResponse)
+def select_one(table_name: str, obj_id: int):
+    return views.TemplateResponse("obj_view.html", {"table": table_name, "obj_id": obj_id})
+
+@app.get('/table={table_name}/select_many', response_class=HTMLResponse)
+def select_many(table_name: str, options: Optional[dict] = None):
+    return views.TemplateResponse("list_view.html", {"table": table_name, "filter": options})
+
+
+
+@app.post('/table={table_name}/insert_one', response_class=HTMLResponse)
+def insert_one(table_name: str, values: dict):
+    return views.TemplateResponse("new_form.html", {"table": table_name, "values": values})
+
+@app.post('/table={table_name}/insert_many', response_class=HTMLResponse)
+def insert_many(table_name: str, amount: int):
+    return views.TemplateResponse("gen_form.html", {"table": table_name, "amount": amount})
+
+
+
+@app.put('/table={table_name}/update_one/id={obj_id}', response_class=HTMLResponse)
+def update_one(table_name: str, obj_id: int, values: dict):
+    return views.TemplateResponse("edit_form.html", {"table": table_name, "obj_id": obj_id, "values":values})
+
+@app.put('/table={table_name}/update_many', response_class=HTMLResponse)
+def update_many(table_name: str, options: Optional[dict], values: dict):
+    return views.TemplateResponse("mod_form.html", {"table": table_name, "options": options, "values": values})
+
+
+
+@app.get("/table/{table_name}/create")
+async def get_insert_form(table_name: str):
+    columns = await get_columns(table_name)
+    return views.TemplateResponse("create_form.html", {"table": table_name, "columns": columns})
+
+
+@app.post("/table/{table_name}/create")
+async def post_insert_data(request: Request, table_name: str, columns: list):
+    data = await request.form()
+    try:
+        await insert_one(table_name, data)
+        # Redirect to success page or list view
+    except ValidationError as e:
+        # Display errors in the form
+        return views.TemplateResponse("create_form.html", {"table": table_name, "columns": columns, "errors": e.errors()})
+    except Exception as e:
+        raise HTTPException(status_code=HTTPStatus(500), detail=str(e))
+
+
+
+
+
+@app.get('/', response_class=HTMLResponse)
+def index(request: Request):
+    return views.TemplateResponse("index.html", {"request": request, "title": "Hello", "body_content": "Hello"})
+
+
+
+
+
+
+if __name__ == "__main__":
+    # def_pool = open_pool("postgres")
+    uvicorn.run("main:app", host='127.0.0.1', port=8272, reload=True)
---
+++ pg_con.py
@@ -0,0 +1,100 @@
+# pg_con.py
+
+import jwt
+import secrets
+from asyncpg import Pool, create_pool, PostgresError
+from datetime import datetime, timedelta
+from fastapi import HTTPException, JSONResponse
+
+KEY = "bf"
+SIGN = "HS256"
+
+con_pools: dict(str, Pool) = {}
+
+
+async def allocate_pool(uname, pword):
+    if not (uname or pword):
+        raise HTTPException('Please provide a credentials')
+    try:
+        pool_str = f'postgres://{uname}:{pword}@localhost:1618/cleaners'
+        con_pools[uname] = await create_pool(pool_str, min_size=1, max_size=6)
+    except Exception as e:
+        raise HTTPException(f"Error creating pool. Info: {e}")
+    return con_pools.get(uname)
+
+
+async def get_con(uname: str = None, pool: Pool = None):
+    if not pool:
+        pool = con_pools.get(uname)
+        if not pool:
+            raise HTTPException('Please allocate a new pool for user')
+    try:
+        con = await pool.acquire()
+        yield con
+    except Exception as e:
+        raise HTTPException(f'Error getting connection. Info: {e}')
+    finally:
+        await pool.release(con)
+
+
+async def test_pool(uname: str = None, pool: Pool = None):
+    return await get_con(uname, pool) is not None
+
+
+async def free_pool(uname):
+    if uname:
+        pool: Pool = con_pools.get(uname)
+        if pool:
+            await pool.close()
+            del con_pools[uname]
+
+
+async def set_cookies(role, uname):
+    expire = datetime.utcnow() + timedelta(minutes=15)
+    jwt_token = jwt.encode({"role": role, "uname": uname, "exp": expire}, KEY, SIGN)
+    csrf_token = secrets.token_urlsafe(16)
+
+    content = {"auth_type": "bearer", "token": jwt_token, "role": role, "uid": uname}
+    response = JSONResponse(content)
+
+    response.set_cookie(key="jwt", value=jwt_token, secure=True, httponly=True, samesite='Lax')
+    response.set_cookie(key="csrf", value=csrf_token, secure=True, httponly=True, samesite='Lax')
+
+    return response
+
+
+async def auth_user(uname, pword, is_guest=False):
+    if is_guest:
+        uname = 'customer'
+    try:
+        await allocate_pool(uname, pword)
+        if uname in ('customer', 'postgres'):
+            is_valid = await test_pool(uname, pword)
+            if is_valid:
+                role = uname
+        else:
+            pool = await init_pool(uname, pword)
+            async for con in gen_con(pool):
+                is_valid = await con.fetchval("select shared.user_validate($1, $2)", uname, pword)
+                if is_valid:
+                    role = await con.fetchval("select shared.user_authorize($1, $2)", uname, pword)
+
+    except PostgresError as e:
+        raise HTTPException(status_code=401, detail=f"Postgres error: {e}")
+    except HTTPException as e:
+        raise HTTPException(status_code=404, detail=f"Token Auth Error: {e}")
+    await create_pool(uname, pword)
+    return await set_cookies(role, uname)
+
+
+async def check_token(token): # token : str = Header(None)
+    try:
+        payload = jwt.decode(token, KEY, [SIGN])
+        role = payload.get("role")
+        uname = payload.get("uname")
+        expire = payload.get("exp")
+        if datetime.utcnow() >= datetime.fromtimestamp(expire):
+            raise HTTPException(403, 'Token is invalid or expired')
+    except jwt.PyJWTError:
+        raise HTTPException(403, 'Auth error. Please try again')
+    return role, uname
