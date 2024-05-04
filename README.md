7d2ba4e7459389841dbdabb30291a25352334d49
mozgoder69 <mozgoder69@gmail.com>
2024-05-04 06:40:37 +0300

    Version 0.2 Summary: Enhance backend functionality and integration readiness
    Details:
    - Added and improved unit tests.
    - Enhanced comments for better code clarity.
    - Refined logging for better traceability.
    - Improved exception handling for robustness.
    - Developed minimal abstractions for DBMS connectivity.
    - Implemented JSON schema extraction from PostgreSQL databases.
    - Generated OpenAPI specifications from JSON schemas.
    
    Conclusion:
    - The backend should now be ready for integration with the frontend.
    
    Next Steps:
    - Focus on frontend development.
    - Create a general README.
    - Review and improve file naming conventions.
    - Evaluate and refine the overall architecture.

--- README.md
+++ README.md
@@ -3,10 +3,24 @@
 This repository contains a prototype of the “Cleaners” information system,
 designed and developed as part of a student's coursework on databases
 
+## Used frameworks and technologies:
+### Domain
+PostgreSql - Database
+AsyncPg - Interface
+
+### API
+Uvicorn - Server (ASGI)
+FastApi - Routes
+Pydantic - Models
+
+### UI
+Jinja2 - Templates
+AlpineJs - Reactivity
+InviseCss - Styling
+
 
 ## [Domain Design](https://github.com/markdown-it/markdown-it-domain)
 
 ## [API Design](https://github.com/markdown-it/markdown-it-api)
 
 ## [UI Design](https://github.com/markdown-it/markdown-it-ui)
-
--- main.py
+++ main.py
@@ -1,130 +1,197 @@
-# Стандартные библиотеки
-from http import HTTPStatus
-from typing import Optional
+# main.py
+# Std libs
+from typing import Annotated, Dict, List, Optional
 
-# Сторонние библиотеки
-from fastapi import FastAPI, HTTPException, Request, Depends
-from fastapi.responses import HTMLResponse, JSONResponse
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import uvicorn
+from app import create_app, setup_openapi, update_openapi
+
+# Local files
+from auth import JWTType, authenticate, decode_token, encode_token, oauth_schema
+from db_con import pools
+from db_fun import funcs
+from dynamic import metadata_to_schema, vJSONSchema
+
+# External modules
+from fastapi import Depends, HTTPException, Request
+from fastapi.responses import JSONResponse
+from fastapi.security import HTTPBearer, OAuth2PasswordRequestForm
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from pydantic import ValidationError
-import uvicorn
+from jose import jwt
+from logs.myrich import log_cfg, logger
 
-# Локальные модули
-from pg_con import get_con, auth_user
-from pg_fun import get_selectable_tables, get_table_columns, get_table_records
-import views
-import domain
+# Instantiate FastAPI app
+app = create_app()
 
-# Создайте основное приложение FastAPI
-app = FastAPI()
-views = Jinja2Templates(directory="views")
-static = StaticFiles(directory="views/static")
-app.mount("/static", static, name="static")
+# Setup security schema
+security = HTTPBearer()
 
-@app.post("/auth")
-async def auth(form_data: OAuth2PasswordRequestForm = Depends(), is_guest=False):
-    # try:
-    #     jwt_token = await auth_user(form_data.username, form_data.password, is_guest)
-    # except Exception as e:
-    #     raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail=f"Incorrect username or password: {e}")
-
-    response = await auth_user(form_data.username, form_data.password, False)
-
-    return response
-
-@app.get("/api/fields/{table_name}")
-async def api_fields(request: Request, table_name):
-    columns_data = await get_columns(table_name)
-    columns = [dict(record) for record in columns_data["table_columns"]]  # Преобразование каждой записи в словарь
-
-    for column in columns:
-        column["html_input_type"] = db_type_to_input(column["ctype"])
-
-    icon_name = db_table_to_icon(table_name)
-
-    # Рендерим HTML-форму с использованием Jinja2
-    return views.TemplateResponse("form_template.html", {"request": request, "icon_name": icon_name, "table_name": table_name, "columns": columns})
+# Mount static files and templates
+app.mount("/static", StaticFiles(directory="static"), "static")
+templates = Jinja2Templates("templates")
 
 
-@app.get("/api/tables")
-async def api_tables():
-    return await get_tables()
-
-@app.get("/api/columns/{table_name}")
-async def api_columns(table_name):
-    return await get_columns(table_name)
-
-@app.get("/api/records/{table_name}")
-async def api_records(table_name):
-    return await get_records(table_name)
+# Initialize and close connection pools on startup/shutdown
+@app.on_event("startup")
+async def startup():
+    await pools.init_pool()
 
 
-#
+@app.on_event("shutdown")
+async def shutdown():
+    await pools.close_all_pools()
 
 
-@app.get('/table={table_name}/select_one/id={obj_id}', response_class=HTMLResponse)
-def select_one(table_name: str, obj_id: int):
-    return views.TemplateResponse("obj_view.html", {"table": table_name, "obj_id": obj_id})
-
-@app.get('/table={table_name}/select_many', response_class=HTMLResponse)
-def select_many(table_name: str, options: Optional[dict] = None):
-    return views.TemplateResponse("list_view.html", {"table": table_name, "filter": options})
-
-
-
-@app.post('/table={table_name}/insert_one', response_class=HTMLResponse)
-def insert_one(table_name: str, values: dict):
-    return views.TemplateResponse("new_form.html", {"table": table_name, "values": values})
-
-@app.post('/table={table_name}/insert_many', response_class=HTMLResponse)
-def insert_many(table_name: str, amount: int):
-    return views.TemplateResponse("gen_form.html", {"table": table_name, "amount": amount})
+# Token generation and refreshing endpoints
+@app.post("/token")
+async def new_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    # logger.debug(form_data.password)
+    role = await authenticate(form_data.username, form_data.password)
+    if not role:
+        raise HTTPException(401, "Invalid credentials", {"WWW-Authenticate": "Bearer"})
+    at = await encode_token(form_data.username, role, JWTType.AT)
+    rt = await encode_token(form_data.username, role, JWTType.RT)
+    return {"access_token": at, "refresh_token": rt, "token_type": "Bearer"}
 
 
-
-@app.put('/table={table_name}/update_one/id={obj_id}', response_class=HTMLResponse)
-def update_one(table_name: str, obj_id: int, values: dict):
-    return views.TemplateResponse("edit_form.html", {"table": table_name, "obj_id": obj_id, "values":values})
-
-@app.put('/table={table_name}/update_many', response_class=HTMLResponse)
-def update_many(table_name: str, options: Optional[dict], values: dict):
-    return views.TemplateResponse("mod_form.html", {"table": table_name, "options": options, "values": values})
-
-
-
-@app.get("/table/{table_name}/create")
-async def get_insert_form(table_name: str):
-    columns = await get_columns(table_name)
-    return views.TemplateResponse("create_form.html", {"table": table_name, "columns": columns})
-
-
-@app.post("/table/{table_name}/create")
-async def post_insert_data(request: Request, table_name: str, columns: list):
-    data = await request.form()
+@app.post("/refresh")
+async def refresh_old_token(refresh_token: Annotated[str, Depends(oauth_schema)]):
     try:
-        await insert_one(table_name, data)
-        # Redirect to success page or list view
-    except ValidationError as e:
-        # Display errors in the form
-        return views.TemplateResponse("create_form.html", {"table": table_name, "columns": columns, "errors": e.errors()})
+        payload = await decode_token(refresh_token, JWTType.RT)
+        if payload.get("type") == "refresh":
+            new_access_token = await encode_token(
+                payload["uname"], payload["role"], JWTType.AT
+            )
+            return {"access_token": new_access_token}
+        raise HTTPException(401, "Invalid refresh token type")
+    except jwt.JWTError as e:
+        raise HTTPException(401, "Invalid token or expired token") from e
+
+
+@app.post("/identity")
+async def identity(access_token: Annotated[str, Depends(oauth_schema)]):
+    payload = await decode_token(access_token, JWTType.AT)
+    logger.debug(payload)
+    if "role" not in payload or "uname" not in payload:
+        raise HTTPException(400, "Invalid token")
+    return payload
+
+
+# API endpoint definitions for database interactions
+@app.get("/tables")
+async def api_tables(access_token: Annotated[dict, Depends(oauth_schema)]):
+    try:
+        payload = await decode_token(access_token, JWTType.AT)
+        return await funcs.get_tables(payload)
     except Exception as e:
-        raise HTTPException(status_code=HTTPStatus(500), detail=str(e))
+        raise HTTPException(500, f"Failed accessing DB: {e}") from e
 
 
+@app.get("/tables/{table}/schema")
+async def table_schema(
+    table,
+    access_token: Annotated[dict, Depends(oauth_schema)],
+    version=vJSONSchema.alpaca,
+):
+    try:
+        payload = await decode_token(access_token, JWTType.AT)
+        metadata = await funcs.get_schema(payload, table)
+        return metadata_to_schema(table, metadata, version)
+    except Exception as e:
+        raise HTTPException(500, f"Failed accessing DB: {e}") from e
 
 
+@app.get("/tables/schemas")
+async def tables_schemas(access_token: Annotated[dict, Depends(oauth_schema)]):
+    schemas = {}
+    tables = await api_tables(access_token)
+    for table in tables:
+        tabname = table.get("table_name")
+        try:
+            schema = await table_schema(
+                tabname, access_token, version=vJSONSchema.openapi
+            )
+            schemas[tabname] = schema  # Используйте имя таблицы как ключ
+        except Exception as e:
+            logger.debug(f"Unsufficient access was prevented. Info: {e}")
+    logger.debug(f"Updating OpenAPI with schemas: {schemas}")
+    update_openapi(app, schemas)
+    return {"schemas": schemas}
 
-@app.get('/', response_class=HTMLResponse)
-def index(request: Request):
-    return views.TemplateResponse("index.html", {"request": request, "title": "Hello", "body_content": "Hello"})
+
+""" Маршруты CRUD """
 
 
+@app.get("/tables/{table}/records/{recordId}")
+async def read_one(table: str, oid: int, payload: dict = Depends(decode_token)):
+    """Получить из таблицы запись по ID"""
+    return JSONResponse(content={"table": table, "oid": oid})
 
 
+@app.get("/tables/{table}/records/list")
+async def list_many(
+    table: str, cond: Optional[dict] = None, payload: dict = Depends(decode_token)
+):
+    """Прочесть из таблицы записи по условию"""
+    data = await funcs.get_records(payload, table, pools)
+    return JSONResponse(content={"table": table, "rule": cond})
 
 
+@app.post("/tables/{table}/records/new")
+async def new_one(table: str, data: Dict, payload: dict = Depends(decode_token)):
+    """Добавить запись с указанными данными в таблицу"""
+    return JSONResponse(content={"table": table, "data": data})
+
+
+@app.post("/tables/{table}/records/bulk")
+async def gen_many(table: str, amount: int, payload: dict = Depends(decode_token)):
+    """Сгенерировать указанное количество записей в таблице"""
+    return JSONResponse(content={"table": table, "amount": amount})
+
+
+@app.put("/tables/{table}/records/{oid}")
+async def edit_one(
+    table: str, oid: int, rule: Dict, payload: dict = Depends(decode_token)
+):
+    """Редактировать в таблице запись по ID"""
+    return JSONResponse(content={"table": table, "oid": oid, "rule": rule})
+
+
+@app.put("/tables/{table}/records/bulk")
+async def upd_many(
+    table: str, rules: List[Dict], payload: dict = Depends(decode_token)
+):
+    """Обновить записи в таблице новыми данными"""
+    return JSONResponse(content={"table": table, "rules": rules})
+
+
+@app.delete("/tables/{table}/records/{oid}")
+async def del_one(table: str, oid: int, payload: dict = Depends(decode_token)):
+    """Удалить записи из таблицы по данному ID"""
+    return JSONResponse(content={"table": table, "oid": oid})
+
+
+@app.delete("/tables/{table}/records/bulk")
+async def trim_many(table: str, oids: List[int], payload: dict = Depends(decode_token)):
+    """Очистить записи в таблице по данным ID"""
+    return JSONResponse(content={"table": table, "oids": oids})
+
+
+@app.get("/home")
+async def index(request: Request):
+    # Нет Depends(decode_token) - домой можно ходить и без логина
+    title = "Welcome To Cleaners API"
+    body_content = "Auth Or Visit /Docs To Explore How To Use API"
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "title": title, "body_content": body_content}
+    )
+
+
+# Setup OpenAPI configuration
+setup_openapi(app)
+
+# Start Uvicorn server if script is run directly
 if __name__ == "__main__":
-    # def_pool = open_pool("postgres")
-    uvicorn.run("main:app", host='127.0.0.1', port=8272, reload=True)
+    uvicorn.run(
+        app, "127.0.0.1", 8272, log_config=log_cfg, log_level="debug", reload=True
+    )
